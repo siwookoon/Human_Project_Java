c81d85b64d53f3e650514c92bb8ff8f76fb4d39b
siwookoon <siwoo537@naver.com>
2023-04-11 16:20:02 +0900

    mollu

--- project/src/main/java/com/human/project/controller/HomeController.java
+++ project/src/main/java/com/human/project/controller/HomeController.java
@@ -4,7 +4,6 @@ package com.human.project.controller;
 
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -13,10 +12,8 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -70,23 +67,24 @@ public class HomeController {
         Map<String, Object> map = principal.getAttributes();
         log.info("map : " + map);
         log.info("map : " + map.get("properties"));
+        if(map.get("properties") != null) {
+			Map<String, Object> proMap = (Map<String, Object>) map.get("properties");
+			Map<String, Object> accountMap = (Map<String, Object>) map.get("kakao_account");
 
-        Map<String, Object> proMap = (Map<String, Object>) map.get("properties");
-        Map<String, Object> accountMap = (Map<String, Object>) map.get("kakao_account");
+			String profile_image = String.valueOf( proMap.get("profile_image") );
+			String thumbnail_image = String.valueOf( proMap.get("thumbnail_image") );
 
-        String profile_image = String.valueOf( proMap.get("profile_image") );
-        String thumbnail_image = String.valueOf( proMap.get("thumbnail_image") );
-
-        String email = String.valueOf( accountMap.get("email") );
+			String email = String.valueOf( accountMap.get("email") );
 
 
-        log.info("map : " + proMap);
-        log.info("email : " + email);
-        log.info("profile_image : " + proMap.get("profile_image"));
+			log.info("map : " + proMap);
+			log.info("email : " + email);
+			log.info("profile_image : " + proMap.get("profile_image"));
 
-        model.addAttribute("email", email);
-        model.addAttribute("profile_image", profile_image);
-        model.addAttribute("thumbnail_image", thumbnail_image);
+			model.addAttribute("email", email);
+			model.addAttribute("profile_image", profile_image);
+			model.addAttribute("thumbnail_image", thumbnail_image);
+        }
       }
 		return "index";
 	}
